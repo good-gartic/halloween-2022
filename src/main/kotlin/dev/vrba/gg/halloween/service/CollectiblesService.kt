30facbbc69167abbc4a5526d6ec11cf8f0ca26ca
Jiří Vrba <jiri@vrba.dev>
2022-10-23 20:51:36 +0200

    Work in progress implementation of the collectibles service

--- src/main/kotlin/dev/vrba/gg/halloween/service/CollectiblesService.kt
+++ src/main/kotlin/dev/vrba/gg/halloween/service/CollectiblesService.kt
@@ -1,4 +1,33 @@
 package dev.vrba.gg.halloween.service
 
-class CollectiblesService {
+import dev.vrba.gg.halloween.domain.Collectible
+import dev.vrba.gg.halloween.domain.Score
+import dev.vrba.gg.halloween.repository.CollectibleRepository
+import dev.vrba.gg.halloween.repository.ScoreRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import java.lang.IllegalArgumentException
+
+@Service
+class CollectiblesService(
+    private val collectibleRepository: CollectibleRepository,
+    private val scoreRepository: ScoreRepository
+) {
+    fun getRandomCollectible(): Collectible {
+        // Weighted random selection
+        val collectible = collectibleRepository.findAllAvailableCollectibles()
+            .flatMap { collectible -> List(collectible.quantity) { collectible } }
+            .random()
+
+        collectibleRepository.save(collectible.copy(quantity = collectible.quantity - 1))
+
+        return collectible
+    }
+
+    fun collectItem(id: Int, user: Long): Score {
+        val collectible = collectibleRepository.findByIdOrNull(id) ?: throw IllegalArgumentException("Cannot find the collectible")
+        val score = scoreRepository.findByUser(user) ?: scoreRepository.save(Score(user = user))
+
+        return scoreRepository.save(score.copy(points =  score.points + collectible.value))
+    }
 }
