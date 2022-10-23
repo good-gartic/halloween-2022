146ba2809ba179608170720b766934ba5906b8b2
Jiří Vrba <jiri@vrba.dev>
2022-10-23 20:10:59 +0200

    Initial commit

---
+++ src/main/kotlin/dev/vrba/gg/halloween/GoodGarticHalloweenApplication.kt
@@ -0,0 +1,11 @@
+package dev.vrba.gg.halloween
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GoodGarticHalloweenApplication
+
+fun main(args: Array<String>) {
+    runApplication<GoodGarticHalloweenApplication>(*args)
+}
---
+++ src/test/kotlin/dev/vrba/gg/halloween/GoodGarticHalloweenApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.vrba.gg.halloween
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GoodGarticHalloweenApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
