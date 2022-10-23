e19710dcea2c7110f669d2fc974cd756c49deb98
Jiří Vrba <jiri@vrba.dev>
2022-10-23 20:30:27 +0200

    Add method for selecting available collectibles

--- src/main/kotlin/dev/vrba/gg/halloween/repository/CollectibleRepository.kt
+++ src/main/kotlin/dev/vrba/gg/halloween/repository/CollectibleRepository.kt
@@ -1,8 +1,14 @@
 package dev.vrba.gg.halloween.repository
 
 import dev.vrba.gg.halloween.domain.Collectible
+import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface CollectibleRepository : CrudRepository<Collectible, Int>
+interface CollectibleRepository : CrudRepository<Collectible, Int> {
+
+    @Query("select * from collectibles where quantity > 0")
+    fun findAllAvailableCollectibles(): List<Collectible>
+
+}
