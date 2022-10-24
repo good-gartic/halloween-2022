7abb0d158a13652d384be0ae96246950c2f890a9
Jiří Vrba <jiri@vrba.dev>
2022-10-24 19:31:35 +0200

    Remove the AOT compilation step to speed up image builds

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,10 @@ plugins {
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
-    id("org.springframework.experimental.aot") version "0.12.1"
 }
 
 group = "dev.vrba"
-version = "0.0.10"
+version = "1.0"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 configurations {
